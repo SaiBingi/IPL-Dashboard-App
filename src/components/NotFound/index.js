36a6d55d2becb25dbd7d24e799efb3d551d43a33
Sai Bingi <saibingi1208@gmail.com>
2022-05-21 03:23:02 +0000

    IPL Dashboard Web Application Solution

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="bg-container">
+    <div className="container">
+      <h1 className="page-not-found">Page Not Found !!</h1>
+    </div>
+  </div>
+)
+
+export default NotFound
---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {teamCardDetails} = props
+  const {id, name, teamImageUrl} = teamCardDetails
+
+  return (
+    <Link to={`/team-matches/${id}`} className="team-link">
+      <li className="team-card-container">
+        <img src={teamImageUrl} alt={`${name}`} className="team-image-url" />
+        <p className="team-name">{name}</p>
+      </li>
+    </Link>
+  )
+}
+
+export default TeamCard
